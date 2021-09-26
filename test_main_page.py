952ee8ef6a3c9ba4e4efbb28123d9638e5d7961c
0Sunray0 <sve126@mail.ru>
2021-09-26 19:22:58 +0300

    added changes to the .py file

--- test_main_page.py
+++ test_main_page.py
@@ -3,11 +3,9 @@ from .pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
